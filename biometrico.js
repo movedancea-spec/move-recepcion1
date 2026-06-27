3c285c949944899ab1987e01d564e9fdd2c07081
movedancea-spec <movedancea@gmail.com>
2026-06-27 04:29:45 -0600

    Update biometrico.js

--- biometrico.js
+++ biometrico.js
@@ -477,8 +477,26 @@ Math.random()*successMessages.length
 
 const ahora = new Date();
 
-registerTime.innerHTML =
-"🕒 "+datos.hora;
+const ahora = new Date();
+
+const hora = ahora.toLocaleTimeString("es-GT",{
+    timeZone:"America/Guatemala",
+    hour:"2-digit",
+    minute:"2-digit",
+    hour12:true
+});
+
+const fecha = ahora.toLocaleDateString("es-GT",{
+    timeZone:"America/Guatemala",
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+});
+
+registerTime.innerHTML = `
+🕒 ${hora}<br>
+📅 ${fecha}
+`;
 
 birthday.innerHTML="";
 
